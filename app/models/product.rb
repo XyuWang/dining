8243a4494f376962538c7f2679c079995ce6a9c6
awsam <xyu.wang.0713@gmail.com>
2013-05-27 13:28:10 +0800

    use paperclip  add avatar to products and stores

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :store_id, :title, :price, :sales_colume
+  attr_accessible :description, :store_id, :title, :price, :sales_colume, :avatar
 
   validates :description, :title, :store, :price, presence: true
 
   belongs_to :store
   has_many :line_items, as: :referable
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
   def can_be_ordered?
     store.opened?
